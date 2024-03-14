3adc13943222047acab2b4b9408d4c3830da862c
Deniel <denielaz2020@gmail.com>
2024-03-14 13:27:50 +0100

    streamlink, get_stream_url

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask, request, jsonify
+import streamlink
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
-# @app.route('/get_stream_url', methods=['GET'])
-# def get_stream_url():
-# 	channel_url = request.args.get('channel')
-# 	if not channel_url:
-# 		return jsonify({'error': 'Параметр channel отсутствует'}), 400
+	return 'Ok'
+	
+@app.route('/get_stream_url', methods=['GET'])
+def get_stream_url():
+	channel_url = request.args.get('channel')
+	if not channel_url:
+		return jsonify({'error': 'Параметр channel отсутствует'}), 400
 
-# 	try:
-# 		streams = streamlink.streams(channel_url)
-# 	except Exception as e:
-# 		return jsonify({'error': 'Ошибка при получении потока: {}'.format(str(e))}), 500
+	try:
+		streams = streamlink.streams(channel_url)
+	except Exception as e:
+		return jsonify({'error': 'Ошибка при получении потока: {}'.format(str(e))}), 500
 
-# 	if streams:
-# 		# Выбираем лучшее качество потока
-# 		best_quality = streams['best']
-# 		return jsonify({'stream_url': best_quality.url})
-# 	else:
-# 		return jsonify({'error': 'Stream не найден.'}), 404
-
-# if __name__ == '__main__':
-# 	app.run(debug=True)
+	if streams:
+		# Выбираем лучшее качество потока
+		best_quality = streams['best']
+		return jsonify({'stream_url': best_quality.url})
+	else:
+		return jsonify({'error': 'Stream не найден.'}), 404
